db97e11e7de860bb326ed9e1c30ec64d31a430ad
shinn-as <78967534+shinn-as@users.noreply.github.com>
2021-05-15 20:19:19 +0900

    Update test.js

--- test.js
+++ test.js
@@ -10,23 +10,23 @@ window.addEventListener('DOMContentLoaded',()=>{
 /* 返答をリクエスト */
 /*---------------------------------------*/
 function handler_request_reply(ev){
-    /*---------------------------------------*/
-    /* コメント取得 */
-    /*---------------------------------------*/
+   
     const comment = document.getElementById('talkapi__input').value;
-
-    /*---------------------------------------*/
-    /* レクエストデータ */
-    /*---------------------------------------*/
+    
+    var newElement = document.createElement("p"); // p要素作成
+    var text="あなた:"+comment
+    var newContent = document.createTextNode(text); // テキストノードを作成
+    newElement.appendChild(newContent); 
+    var parentDiv = document.getElementById("honbun");
+    // 追加
+    parentDiv.appendChild(newElement)
+   
     let formdata = new FormData();
     //- apikeyパラメーター 
     formdata.append('apikey','DZZff02h0tFvmNIDQVZKg6PHxY0hlsWu');
     //- コメント
     formdata.append('query',comment);
 
-    /*---------------------------------------*/
-    /* リクエスト */
-    /*---------------------------------------*/
     fetch('https://api.a3rt.recruit-tech.co.jp/talk/v1/smalltalk',{
         method: 'post',
         body: formdata,
@@ -35,8 +35,17 @@ function handler_request_reply(ev){
         response.json().then(data => {
             //- 返答取得
             const reply = data.results[0].reply;
+  
+  
+            var pcreate = document.createElement("p"); // p要素作成
+            var text2="AI:"+reply;
+            var newContent2 = document.createTextNode(text2); // テキストノードを作成
+            pcreate.appendChild(newContent2);
+            var parentDiv = document.getElementById("honbun");
+            // 追加
+            parentDiv.appendChild(pcreate);
             //- 出力
-            document.getElementById('talkapi__reply').innerHTML = reply;
+            //document.getElementById('talkapi__reply').innerHTML = reply;
         });
     });
 
